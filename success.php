fd174214ae568f7bd479d632383d2a9180df4db0
IvanSimic95 <email@isimic.com>
2022-02-02 23:46:41 +0100

    gdas

--- success.php
+++ success.php
@@ -17,7 +17,7 @@ include $_SERVER['DOCUMENT_ROOT'].'/config/vars.php';
     if($result->num_rows != 0) {
       $row = $result->fetch_assoc();
 
-    echo $row['0']['genderAcc'];
+    echo $row['genderAcc'];
      
    
   }
