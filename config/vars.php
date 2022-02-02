6ab1851e97f7630ee6c1c90ecff56ae8ad580546
IvanSimic95 <email@isimic.com>
2022-02-02 20:20:20 +0100

    .

--- config/vars.php
+++ config/vars.php
@@ -47,6 +47,11 @@ if($domain == "melissa.test"){
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
+$conn->query('set character_set_client=utf8');
+$conn->query('set character_set_connection=utf8');
+$conn->query('set character_set_results=utf8');
+$conn->query('set character_set_server=utf8');
+$conn->set_charset('utf8mb4');
 
 // Check connection
 if ($conn->connect_error) {die("Connection failed: " . $conn->connect_error);}
