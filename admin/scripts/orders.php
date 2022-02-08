fc010e457900c1af1e27d1a32e42547acc78e1ac
IvanSimic95 <email@isimic.com>
2022-02-08 05:32:46 +0100

    dsa

--- admin/scripts/orders.php
+++ admin/scripts/orders.php
@@ -30,7 +30,7 @@ $sql = "SELECT * FROM orders WHERE order_status = 'processing' OR order_status =
                         <td>' . $row["user_name"]. '</td>
                         <td>' . $row["order_email"]. '</td>
                         <td>' . $product. '</td>
-                        <td><span class="sbadge sbadge-' . $status. '">' . $status. '</span></td>
+                        <td><span class="sbadge sbadge-' . $status. '">' . $status. ' <i class="fas fa-check"></i><i class="fas fa-stream"></i><i class="fas fa-redo"></i><i class="fas fa-ban"></i></span></td>
                         <td>$' . $row["order_price"]. '</td>
                         <td>' . $row["order_priority"]. '</td>
                         </tr>
@@ -39,3 +39,46 @@ $sql = "SELECT * FROM orders WHERE order_status = 'processing' OR order_status =
                         $conn->close();
                                 }
                                 ?>
+<style>
+span.sbadge{
+    border-radius: 0.25rem !important;
+    padding: 0.5rem !important;
+    text-transform: capitalize!important;
+    font-weight:600!important;
+    text-align: center;
+  }
+  
+  span.sbadge-pending{
+  color: #9d5228;
+  background-color: #fde6d8;
+  }
+  span.sbadge-completed{
+  color: #00864e;
+  background-color: #ccf6e4;
+  }
+  span.sbadge-processing{
+  color: #1c4f93;
+  background-color: #d5e5fa;
+  }
+  span.sbadge-canceled{
+  color: #7d899b;
+  background-color: #e3e6ea;
+  }
+  
+  
+  span.sbadge .fas{
+  display:none;
+  }
+  span.sbadge-completed .fa-check{
+  display:inline-block;
+  }
+  span.sbadge-processing .fa-redo{
+  display:inline-block;
+  }
+  span.sbadge-pending .fa-stream{
+  display:inline-block;
+  }
+  span.sbadge-canceled .fa-ban{
+  display:inline-block;
+  }
+</style>
