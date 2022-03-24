5135bdbbc269fa639b891adbea5c74dcbd8f37bb
IvanSimic95 <email@isimic.com>
2022-03-24 19:49:28 +0100

    fsaf

--- webhook/format.php
+++ webhook/format.php
@@ -2,7 +2,7 @@
 if(isset($_GET['code'])){
     $code = $_GET['code'];
 
-    $pieces = explode(", ", $code);
+    $pieces = explode(",", $code);
     $splitCode = $pieces[0];
 
     $finishedCode = preg_replace('/[0-9]+/', '', $splitCode);
