7404e235dc51df3ad35e46403304a35015edd382
IvanSimic95 <email@isimic.com>
2022-03-29 02:59:25 +0200

    hafhadfsh

--- assets/order/func/complete-orders.php
+++ assets/order/func/complete-orders.php
@@ -239,11 +239,9 @@ $logArray['2'] = $_SERVER['REMOTE_ADDR'];
 		//If trigger is set to 1 (order is ready to be delivered)
 		if ($trigger == 1) {
 			$logArray[] = $sql_text;
-			echo $sql_text;
+			$logArray[] = $rowText["id"];
+			echo $rowText["id"];
 			$message = str_replace("%FIRSTNAME%", $fName, $message);
-			echo $message;
-			$logArray[] = $message;
-			$logArray[] = $ContinueConvoMsg;
 			if ($image_send == "1") { //SEND IMAGE START
 						// define image name and new path
 							$rootDir = $_SERVER['DOCUMENT_ROOT'];
