421376781ce2b69f846873d08cde538d1dcee337
IvanSimic95 <86245505+IvanSimic95@users.noreply.github.com>
2022-04-07 23:44:43 +0200

    Update start-orders.php

--- assets/order/func/start-orders.php
+++ assets/order/func/start-orders.php
@@ -75,6 +75,29 @@ echo "Starting start-orders.php...<br><br>";
 			if ($conn->query($sqlupdate) === TRUE) {
       		echo "Updated";
 
+
+
+$ch = curl_init();
+$data = [
+"custom" => "",
+
+];
+$data1 = json_encode($data);
+print_r($data1);
+curl_setopt($ch, CURLOPT_URL, 'https://melissa-psychic.com/fix-chat.php?order='.$orderId.'&name='.$customerName.'&email='.$orderEmail.'&product='.$orderProduct.'&codename='.$orderProduct);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
+$headers = array();
+$headers[] = 'Content-Type: application/json';
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+
+$result = curl_exec($ch);
+if (curl_errno($ch)) {
+    echo 'Error:' . curl_error($ch);
+}
+curl_close($ch);
+//Change chat order status
+
 			// curl implementation
 $ch = curl_init();
 $data = [
