f5dfb141b48fc079bcf5358fe15aab068e36b768
s0mekind <gabeobm@gmail.com>
2022-04-21 19:11:01 +0300

    Revert "Update readings.php"
    This reverts commit 95f11bb4335fd061e1c93b18375911b621d06094.

--- readings.php
+++ readings.php
@@ -196,14 +196,7 @@ text-align:center;
 </div>
 
 
-<!-- Twitter single-event website tag code -->
-<script src="//static.ads-twitter.com/oct.js" type="text/javascript"></script>
-<script type="text/javascript">twttr.conversion.trackPid('o8ifi', { tw_sale_amount: 0, tw_order_quantity: 0 });</script>
-<noscript>
-<img height="1" width="1" style="display:none;" alt="" src="https://analytics.twitter.com/i/adsct?txn_id=o8ifi&p_id=Twitter&tw_sale_amount=0&tw_order_quantity=0" />
-<img height="1" width="1" style="display:none;" alt="" src="//t.co/i/adsct?txn_id=o8ifi&p_id=Twitter&tw_sale_amount=0&tw_order_quantity=0" />
-</noscript>
-<!-- End Twitter single-event website tag code -->
+
 
 <script>
       var $checkboxes = $('.list-group-control input[type="checkbox"]');
