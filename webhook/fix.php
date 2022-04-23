1e94cab9c6f74c81085f2bac6d15dc4baab5fa18
IvanSimic95 <email@isimic.com>
2022-04-23 05:17:55 +0200

    Update fix.php

--- webhook/fix.php
+++ webhook/fix.php
@@ -3,7 +3,7 @@ include_once $_SERVER['DOCUMENT_ROOT'].'/config/vars.php';
 include_once $_SERVER['DOCUMENT_ROOT'].'/vendor/autoload.php';
 $parser = new TheIconic\NameParser\Parser();
 
-$sql = "SELECT * FROM orders WHERE first_name = '' ORDER BY order_id DESC LIMIT 25";
+$sql = "SELECT * FROM orders WHERE first_name = '' ORDER BY order_id DESC LIMIT 100";
 $sqlResoult = $conn->query($sql);
 
 	if($sqlResoult->num_rows == 0) {
