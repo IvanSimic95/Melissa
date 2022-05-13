aff3342ac233d9a244a8dd31aceed612b383cf64
s0mekind <gabeobm@gmail.com>
2022-05-13 10:26:32 +0300

    Update header.php

--- assets/templates/header.php
+++ assets/templates/header.php
@@ -25,6 +25,17 @@ $metatags
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
 <head>
+<!-- Hotjar Tracking Code for https://melissa-psychic.com -->
+<script>
+    (function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:2968253,hjsv:6};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+</script>
 
 <!-- Global site tag (gtag.js) - Google Analytics -->
 <script async src="https://www.googletagmanager.com/gtag/js?id=UA-227896344-1"></script>
