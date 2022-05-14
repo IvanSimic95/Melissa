d02f56089e7a4cc89c32c5ac9f737fbefbb7b909
s0mekind <gabeobm@gmail.com>
2022-05-14 10:36:38 +0300

    Update order-complete.php

--- order-complete.php
+++ order-complete.php
@@ -72,7 +72,7 @@ if(isset($_POST['form_submit'])){
 <script>
     (function(h,o,t,j,a,r){
         h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-        h._hjSettings={hjid:2968579,hjsv:6};
+        h._hjSettings={hjid:2968253,hjsv:6};
         a=o.getElementsByTagName('head')[0];
         r=o.createElement('script');r.async=1;
         r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
