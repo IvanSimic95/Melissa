ce54cfc9566e753c41bf793ff15576cd799b2d9d
IvanSimic95 <email@isimic.com>
2022-10-02 21:56:13 +0200

    a

--- test.php
+++ test.php
@@ -2,10 +2,14 @@
 
 require $_SERVER['DOCUMENT_ROOT'].'/vendor/autoload.php';
 
+$pass1 = "Adasu8oazst";
+$pass2 = "ete21435";
+$pass = $pass1.$pass2;
+
 // Create the Transport
 $transport = (new Swift_SmtpTransport('smtp.gmail.com', 465, "ssl"))
   ->setUsername('contact@melissa-psychic.com')
-  ->setPassword('Dadada123!')
+  ->setPassword($pass)
 ;
 
 // Create the Mailer using your created Transport
@@ -22,7 +26,7 @@ $email_body = "You have received a new message from your soulmate-psychic.com co
 // Create a message
 $message = (new Swift_Message($email_subject))
   ->setFrom(['contact@melissa-psychic.com' => 'Ivan'])
-  ->setTo(['contact@soulmate-psychic.com'])
+  ->setTo(['contact@melissa-psychic.com'])
   ->setReplyTo([$email_address])
   ->setBody($email_body)
   ;
