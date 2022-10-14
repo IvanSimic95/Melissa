cfe6683426e6d16ac27f04b349534fc068cf2420
IvanSimic95 <email@isimic.com>
2022-10-14 15:02:54 +0200

    Update order.php

--- ajax/order.php
+++ ajax/order.php
@@ -169,7 +169,7 @@ $submitStatus = "Success";
 $SuccessMessage = "Information saved, Redirecting you to Payment Page Now!";
 $redirectPayment = "https://melissapsy.pay.clickbank.net/?cbskin=39040&cbtimer=1593&cbfid=52316&cbitems=".$cbproduct."&name=".$user_name."&email=".$user_email."&cookie_ID=".$cookie_id."&order_ID=".$lastRowInsert."&main_ID=".$lastRowInsert;
 
-$recoverPayment = "https://melissapsy.pay.clickbank.net/?cbskin=39040&cbtimer=1661&cbfid=52316&cbitems=".$cbproduct."&name=".$user_name."&email=".$user_email."&cookie_ID=".$cookie_id."&order_ID=".$lastRowInsert."&main_ID=".$lastRowInsert."&email=recovery&coupon=KM6CMMI";
+$recoverPayment = "https://melissapsy.pay.clickbank.net/?cbskin=39040&cbtimer=1661&cbfid=52316&cbitems=".$cbproduct."&name=".$user_name."&email=".$user_email."&cookie_ID=".$cookie_id."&order_ID=".$lastRowInsert."&main_ID=".$lastRowInsert."&femail=recovery&coupon=KM6CMMI";
 
 $sqlupdate = "UPDATE `orders` SET `cart_recover`='$recoverPayment' WHERE order_id='$lastRowInsert'";
 if ($conn->query($sqlupdate) === TRUE) {
