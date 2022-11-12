02999ab4891749578bd4efacc157f4e57e771058
IvanSimic95 <email@isimic.com>
2022-11-12 06:58:42 +0100

    Revert "Revert "a""
    This reverts commit 20ff57e0b9df0900bff00f5f9c12d3c06115b68b.

--- admin/scripts/campaigns.php
+++ admin/scripts/campaigns.php
@@ -23,13 +23,13 @@
                                         }
 
                                         //Find campaign Sales Count from DB
-                                        $sql4 = "SELECT * FROM orders WHERE (order_status = 'shipped' AND fbCampaign = '$id' AND DATE(order_date) >= '$startDate' AND DATE(order_date) <= '$endDate') OR (order_status = 'processing' AND fbCampaign = '$id' AND DATE(order_date) >= '$startDate' AND DATE(order_date) <= '$endDate')";
+                                        $sql4 = "SELECT * FROM orders WHERE (order_status = 'shipped' AND order_product = 'soulmate' AND fbCampaign = '$id' AND DATE(order_date) >= '$startDate' AND DATE(order_date) <= '$endDate') OR (order_status = 'processing' AND order_product = 'soulmate' AND fbCampaign = '$id' AND DATE(order_date) >= '$startDate' AND DATE(order_date) <= '$endDate')";
                                         $r4 = $conn->query($sql4);
                                         $countSales = $r4->num_rows;
                                   
 
                                         //Find campaign Sales from DB
-                                        $sql3 = "SELECT SUM(order_price) AS sum_quantity FROM orders WHERE (order_status = 'shipped' AND fbCampaign = '$id' AND DATE(order_date) >= '$startDate' AND DATE(order_date) <= '$endDate') OR (order_status = 'processing' AND fbCampaign = '$id' AND DATE(order_date) >= '$startDate' AND DATE(order_date) <= '$endDate')";
+                                        $sql3 = "SELECT SUM(order_price) AS sum_quantity FROM orders WHERE (order_status = 'shipped' AND order_product = 'soulmate' AND fbCampaign = '$id' AND DATE(order_date) >= '$startDate' AND DATE(order_date) <= '$endDate') OR (order_status = 'processing' AND order_product = 'soulmate' AND fbCampaign = '$id' AND DATE(order_date) >= '$startDate' AND DATE(order_date) <= '$endDate')";
                                         $r3 = $conn->query($sql3);
                                         $fetch3 = $r3->fetch_assoc();
                                         $sum = $fetch3['sum_quantity'];
