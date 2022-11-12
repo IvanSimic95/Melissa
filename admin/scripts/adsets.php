fc769b751a1c62667f026e318dbc2a505b264cc4
IvanSimic95 <email@isimic.com>
2022-11-12 06:57:00 +0100

    a

--- admin/scripts/adsets.php
+++ admin/scripts/adsets.php
@@ -1,7 +1,7 @@
 <?php
 
 
-        $sql = "SELECT * FROM orders WHERE (order_status = 'shipped' AND fbCampaign = '$campaign') OR (order_status = 'processing' AND fbCampaign = '$campaign') GROUP BY fbAdset ORDER BY order_id DESC";
+        $sql = "SELECT * FROM orders WHERE (order_status = 'shipped' AND order_product = 'soulmate' AND fbCampaign = '$campaign') OR (order_status = 'processing' AND order_product = 'soulmate' AND fbCampaign = '$campaign') GROUP BY fbAdset ORDER BY order_id DESC";
         $result = $conn->query($sql);
                 if ($result->num_rows == 0) {
                          echo "no results";
@@ -23,13 +23,13 @@
                                         }
 
                                         //Find campaign Sales Count from DB
-                                        $sql4 = "SELECT * FROM orders WHERE (order_status = 'shipped' AND fbCampaign = '$campaign' AND fbAdset = '$id' AND DATE(order_date) >= '$startDate' AND DATE(order_date) <= '$endDate') OR (order_status = 'processing' AND fbCampaign = '$campaign' AND fbAdset = '$id' AND DATE(order_date) >= '$startDate' AND DATE(order_date) <= '$endDate')";
+                                        $sql4 = "SELECT * FROM orders WHERE (order_status = 'shipped' AND order_product = 'soulmate' AND fbCampaign = '$campaign' AND fbAdset = '$id' AND DATE(order_date) >= '$startDate' AND DATE(order_date) <= '$endDate') OR (order_status = 'processing' AND order_product = 'soulmate' AND fbCampaign = '$campaign' AND fbAdset = '$id' AND DATE(order_date) >= '$startDate' AND DATE(order_date) <= '$endDate')";
                                         $r4 = $conn->query($sql4);
                                         $countSales = $r4->num_rows;
                                   
 
                                         //Find campaign Sales from DB
-                                        $sql3 = "SELECT SUM(order_price) AS sum_quantity FROM orders WHERE (order_status = 'shipped' AND fbCampaign = '$campaign' AND fbAdset = '$id' AND DATE(order_date) >= '$startDate' AND DATE(order_date) <= '$endDate') OR (order_status = 'processing' AND fbCampaign = '$campaign' AND fbAdset = '$id' AND DATE(order_date) >= '$startDate' AND DATE(order_date) <= '$endDate')";
+                                        $sql3 = "SELECT SUM(order_price) AS sum_quantity FROM orders WHERE (order_status = 'shipped' AND order_product = 'soulmate' AND fbCampaign = '$campaign' AND fbAdset = '$id' AND DATE(order_date) >= '$startDate' AND DATE(order_date) <= '$endDate') OR (order_status = 'processing' AND order_product = 'soulmate' AND fbCampaign = '$campaign' AND fbAdset = '$id' AND DATE(order_date) >= '$startDate' AND DATE(order_date) <= '$endDate')";
                                         $r3 = $conn->query($sql3);
                                         $fetch3 = $r3->fetch_assoc();
                                         $sum = $fetch3['sum_quantity'];
