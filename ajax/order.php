02061c6b7a0910d63d7931b43ced3b9d367b664e
IvanSimic95 <email@isimic.com>
2022-11-14 14:19:09 +0100

    Update order.php

--- ajax/order.php
+++ ajax/order.php
@@ -24,6 +24,8 @@ $interval = $birthday->diff(new DateTime);
 
 $user_age = $interval->y;
 
+$pixel_number = $_POST['pixel_number'];
+
 
 $user_name = $_POST['form_name'];
 $user_name = htmlspecialchars($user_name, ENT_QUOTES);
@@ -160,8 +162,8 @@ case "Twinflame":
                     break;
 }
 
-$sql = "INSERT INTO orders (cookie_id, user_age, first_name, last_name, user_name, birthday, order_status, order_date, order_email, order_product, order_product_nice, order_priority, order_price, buygoods_order_id, user_sex, genderAcc, pick_sex, user_agent, user_ip, fbc, fbp, fbCampaign, fbAdset, fbAd, affid, s1, s2) 
-VALUES ('$cookie_id', '$user_age', '$fName', '$lName', '$user_name', '$user_birthday', '$oStatus', '$order_date', '$user_email', '$order_product', '$order_product_nice', '$order_priority', '$cbprice', '', '$userGender', '$userGenderAcc', '$partnerGender', '$user_agent', '$user_ip', '$uFBC', '$uFBP', '$fbCampaign', '$fbAdset', '$fbAd', '$newaffid', '$s1', '$s2')";
+$sql = "INSERT INTO orders (cookie_id, user_age, first_name, last_name, user_name, birthday, order_status, order_date, order_email, order_product, order_product_nice, order_priority, order_price, buygoods_order_id, user_sex, genderAcc, pick_sex, user_agent, user_ip, pixel, fbc, fbp, fbCampaign, fbAdset, fbAd, affid, s1, s2) 
+VALUES ('$cookie_id', '$user_age', '$fName', '$lName', '$user_name', '$user_birthday', '$oStatus', '$order_date', '$user_email', '$order_product', '$order_product_nice', '$order_priority', '$cbprice', '', '$userGender', '$userGenderAcc', '$partnerGender', '$user_agent', '$user_ip', '$pixel_number', '$uFBC', '$uFBP', '$fbCampaign', '$fbAdset', '$fbAd', '$newaffid', '$s1', '$s2')";
 
 if(mysqli_query($conn,$sql)){
 $lastRowInsert = mysqli_insert_id($conn);
